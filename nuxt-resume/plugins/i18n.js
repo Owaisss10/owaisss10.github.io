54fc8a5c38931818efe2c0350888a2b4d7e947b0
Awais Akram <owaisakram103@gmail.com>
2023-12-03 22:34:40 +0200

    nuxt-resume folder deleted

--- nuxt-resume/plugins/i18n.js
+++
@@ -1,8 +0,0 @@
-// ~/plugins/myplugin.js
-
-export default ({ app }) => {
-  // Get localized path for homepage
-  const localePath = app.localePath('index')
-  // Get path to switch current route to language code FI?
-  // const switchLocalePath = app.switchLocalePath('<fi>')
-}
