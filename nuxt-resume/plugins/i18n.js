b9a08ddb5415ed622d30db98e401970e0a31205f
Awais Akram <owaisakram103@gmail.com>
2023-12-03 22:09:30 +0200

    Nuxt resume files added

---
+++ nuxt-resume/plugins/i18n.js
@@ -0,0 +1,8 @@
+// ~/plugins/myplugin.js
+
+export default ({ app }) => {
+  // Get localized path for homepage
+  const localePath = app.localePath('index')
+  // Get path to switch current route to language code FI?
+  // const switchLocalePath = app.switchLocalePath('<fi>')
+}
